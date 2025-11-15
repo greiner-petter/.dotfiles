eda57d43a8548cffd59b6c1e1daeffa2c911ba6b
greiner-petter <github@greiner-petter.net>
2025-11-15 14:46:22 +0100

    initial commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,18 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+alias autoremove='pacman -Rcns $(pacman -Qdtq)'
+
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if read -z cwd <"$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+
+end
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
