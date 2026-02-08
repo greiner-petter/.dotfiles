f97bef1a994308ec47c59e169b962fb570f90c05
greiner-petter <github@greiner-petter.net>
2026-02-08 21:57:36 +0100

    fix theme colors

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,8 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 
+# Custom colours
+cat ~/.local/state/caelestia/sequences.txt 2> /dev/null
+
 # overwrite greeting
 # potentially disabling fastfetch
 #function fish_greeting
