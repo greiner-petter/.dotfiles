01eeee97600ab14249d1d8c5714ea57d8b5f0c47
greiner-petter <github@greiner-petter.net>
2026-02-09 12:00:10 +0100

    added package management

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,13 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 # Custom colours
 cat ~/.local/state/caelestia/sequences.txt 2> /dev/null
 
+# Save package lists to dotfiles
+function pkg-save
+    pacman -Qen | awk '{print $1}' > ~/.dotfiles/packages/pacman.txt
+    pacman -Qem | awk '{print $1}' > ~/.dotfiles/packages/aur.txt
+    echo "Package lists saved to ~/.dotfiles/packages/"
+end
+
 # overwrite greeting
 # potentially disabling fastfetch
 #function fish_greeting
