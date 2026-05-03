5d0fe5f2959b24571ef4e1437e33f0dc8d43ad94
greiner-petter <github@greiner-petter.net>
2026-05-03 16:21:48 +0200

    remove some packages + pull quickshell from aur

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,8 +5,14 @@ cat ~/.local/state/caelestia/sequences.txt 2> /dev/null
 
 # Save package lists to dotfiles
 function pkg-save
-    pacman -Qen | awk '{print $1}' > ~/.dotfiles/packages/pacman.txt
+    # Packages in official/CachyOS repos that should be installed from AUR
+    set aur_exceptions quickshell-git
+
+    pacman -Qen | awk '{print $1}' | grep -Evx (string join '|' $aur_exceptions) > ~/.dotfiles/packages/pacman.txt
     pacman -Qem | awk '{print $1}' > ~/.dotfiles/packages/aur.txt
+    for pkg in $aur_exceptions
+        pacman -Qq $pkg &>/dev/null && echo $pkg >> ~/.dotfiles/packages/aur.txt
+    end
     echo "Package lists saved to ~/.dotfiles/packages/"
 end
 
