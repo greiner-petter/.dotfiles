fefa67abbbccdffc6592a08e3e7b9bedb7507044
greiner-petter <github@greiner-petter.net>
2026-05-17 18:59:01 +0200

    use default fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,23 +1,45 @@
-source /usr/share/cachyos-fish-config/cachyos-config.fish
+if status is-interactive
+    # Starship custom prompt
+    starship init fish | source
 
-# Custom colours
-cat ~/.local/state/caelestia/sequences.txt 2> /dev/null
+    # Direnv + Zoxide
+    command -v direnv &> /dev/null && direnv hook fish | source
+    command -v zoxide &> /dev/null && zoxide init fish --cmd cd | source
 
-# Save package lists to dotfiles
-function pkg-save
-    # Packages in official/CachyOS repos that should be installed from AUR
-    set aur_exceptions quickshell-git
+    # Better ls
+    alias ls='eza --icons --group-directories-first -1'
 
-    pacman -Qen | awk '{print $1}' | grep -Evx (string join '|' $aur_exceptions) > ~/.dotfiles/packages/pacman.txt
-    pacman -Qem | awk '{print $1}' > ~/.dotfiles/packages/aur.txt
-    for pkg in $aur_exceptions
-        pacman -Qq $pkg &>/dev/null && echo $pkg >> ~/.dotfiles/packages/aur.txt
+    # Abbrs
+    abbr lg 'lazygit'
+    abbr gd 'git diff'
+    abbr ga 'git add .'
+    abbr gc 'git commit -am'
+    abbr gl 'git log'
+    abbr gs 'git status'
+    abbr gst 'git stash'
+    abbr gsp 'git stash pop'
+    abbr gp 'git push'
+    abbr gpl 'git pull'
+    abbr gsw 'git switch'
+    abbr gsm 'git switch main'
+    abbr gb 'git branch'
+    abbr gbd 'git branch -d'
+    abbr gco 'git checkout'
+    abbr gsh 'git show'
+
+    abbr l 'ls'
+    abbr ll 'ls -l'
+    abbr la 'ls -a'
+    abbr lla 'ls -la'
+
+    # Custom colours
+    cat ~/.local/state/caelestia/sequences.txt 2> /dev/null
+
+    # For jumping between prompts in foot terminal
+    function mark_prompt_start --on-event fish_prompt
+        echo -en "\e]133;A\e\\"
     end
-    echo "Package lists saved to ~/.dotfiles/packages/"
-end
 
-# overwrite greeting
-# potentially disabling fastfetch
-#function fish_greeting
-#    # smth smth
-#end
+    # Custom fish config
+    source ~/.config/caelestia/user-config.fish 2> /dev/null
+end
