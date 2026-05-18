7932d4305b30bb06eca13045b0e41225c4b08958
greiner-petter <github@greiner-petter.net>
2026-05-18 19:12:41 +0200

    add caelestia submodule

---
+++ stow.sh
@@ -0,0 +1,15 @@
+stow --target="$HOME/.config" \
+    --ignore='\.github' \
+    --ignore='btop' \
+    --ignore='fastfetch' \
+    --ignore='spicetify' \
+    --ignore='thunar' \
+    --ignore='vscode' \
+    --ignore='zed' \
+    --ignore='zen' \
+    --ignore='install\.fish' \
+    --ignore='PKGBUILD' \
+    --ignore='README\.md' \
+    --ignore='starship\.toml' \
+    caelestia
+stow .
