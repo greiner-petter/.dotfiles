e262a7eafe9a6e4264a639f61438dc64cf831b19
greiner-petter <github@greiner-petter.net>
2026-06-23 18:49:44 +0200

    switch to lua config

---
+++ desktop/.config/caelestia/hypr-user.lua
@@ -0,0 +1,51 @@
+local vars = require("variables")
+
+-- Monitor config
+hl.monitor({
+    output   = "",
+    mode     = "2560x1440@240",
+    position = "auto",
+    scale    = 1,
+})
+
+-- Hyprland config
+hl.config({
+    input = {
+        kb_layout = "eu",
+        kb_options = "caps:escape,altwin:swap_lalt_lwin",
+
+        force_no_accel = true,
+
+        scroll_method = "on_button_down",
+        scroll_button = 274,
+
+        float_switch_override_focus = 2,
+    },
+    misc = {
+        vrr = 3
+    }
+})
+
+-- Window rules
+hl.window_rule({ match = { class = "xarchiver" }, tag = "+float" })
+hl.window_rule({ match = { class = "claude-desktop" }, workspace = "special:claude" })
+
+-- Shell keybinds
+hl.unbind("SUPER + SUPER_L")
+hl.bind("SUPER + Space", hl.dsp.global("caelestia:launcher"))
+
+hl.unbind("SUPER + ALT + S")
+hl.bind("SUPER + ALT + S", hl.dsp.global("caelestia:screenshotFreeze"))
+
+hl.unbind("SUPER + SHIFT + S")
+hl.bind("SUPER + SHIFT + S", hl.dsp.window.move({ workspace = "special:special" }))
+
+hl.unbind("CTRL + ALT + V")
+hl.bind("SUPER + Return", hl.dsp.exec_cmd(vars.terminal))
+
+hl.bind("SUPER + C", hl.dsp.exec_cmd("caelestia toggle claude"))
+
+-- Environment variables
+hl.env("LIBVA_DRIVER_NAME", "nvidia")
+hl.env("NVD_BACKEND", "direct")
+hl.env("MOZ_DISABLE_RDD_SANDBOX", 1)
---
+++ desktop/.config/caelestia/hypr-vars.lua
@@ -0,0 +1,40 @@
+return {
+    ------------------
+    ---- HYPRLAND ----
+    ------------------
+
+    -- Apps
+    editor                 = "zeditor",
+    fileExplorer           = "pcmanfm-qt",
+
+    -- Gaps
+    workspaceGaps          = 10,
+    singleWindowGapsOut    = 10,
+
+    -- Window styling
+    windowBorderSize       = 3,
+
+    -- Misc
+    volumeStep             = 1,
+    cursorTheme            = "WhiteSur-cursors",
+
+    ------------------
+    ---- KEYBINDS ----
+    ------------------
+
+    -- Workspaces
+    kbMoveWinToWs          = "SUPER + SHIFT",
+    kbMoveWinToWsGroup     = "CTRL + SUPER + SHIFT",
+
+    -- Window Action
+    kbToggleWindowFloating = "SUPER + SHIFT + F",
+
+    -- Apps
+    kbBrowser              = "SUPER + B",
+    kbEditor               = "SUPER + E",
+    kbFileExplorer         = "SUPER + N",
+
+    -- Misc
+    kbSession              = "SUPER + SHIFT + Escape",
+    kbShowSidebar          = "SUPER + A",
+}
